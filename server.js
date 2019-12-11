e2a13e6dc82ed5360b1e3df4bf3bb2fd9eb321bc
juyalpriyank <juyalpriyank@gmail.com>
2019-12-11 17:55:45 +0530

    faker added

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var app = express()
 var dateTime = require('node-datetime')
 var datetime = dateTime.create()
 var bodyParser = require('body-parser')
+var faker = require('faker');
 
-app.use( bodyParser.json() )
+
+app.use(bodyParser.json())
 
 mongodb.connect(uri, {useUnifiedTopology: true}, function(err, client){
     if (err) throw err
@@ -14,70 +16,108 @@ mongodb.connect(uri, {useUnifiedTopology: true}, function(err, client){
     var commentColl = db.collection('comments')
     var memberColl = db.collection('members')
     var trashCommentColl = db.collection('trash_comments')
+    var organisationsColl = db.collection('organisations')
     console.log("Database created")
     app.locals.commentColl = commentColl
     app.locals.memberColl = memberColl
     app.locals.trashCommentColl = trashCommentColl
+    app.locals.organisationsColl = organisationsColl
 })
 
-function insert_comment(req, org_name, comment){
+
+// TODO: Call control c function to close db conn
+
+async function populateOrgData(req, org_name){
+    var coll = req.app.locals.organisationsColl
+    numOfRecords = Math.floor(Math.random() * Math.floor(5)) + 1
+    for(var i = numOfRecords; i<=numOfRecords; i++){
+        myObj = {"name": faker.name.findName(),
+                 "org_name": org_name,
+                 "followers": faker.random.number(),
+                 "following": faker.random.number(),
+                 "email": faker.internet.email(),
+                 "avatar": faker.internet.avatar()
+                }
+        await coll.insertOne(myObj)
+    }
+}
+
+async function orgExists(req, org_name){
+    var coll = req.app.locals.organisationsColl
+    var upsertVal = await coll.updateOne({"org_name": org_name}, {$setOnInsert: {"org_name": org_name}}, {upsert: true})
+    console.log("printing t:-", t.result)
+    if (upsertVal.result.upserted){
+        console.log("upserted")
+        await populateOrgData(req, org_name)
+    }
+}
+
+async function insert_comment(req, org_name, comment){
     var coll = req.app.locals.commentColl
     var formatted = datetime.format('d-m-y H:M:S')
     var doc = {"comment": comment, "org_name": org_name, "created": formatted}
-    coll.insertOne(doc, function(err, res) {
+    await coll.insertOne(doc, function(err, res) {
         if (err) throw err
         console.log("1 document inserted")
       })
+    await orgExists(req, org_name)
 }
 
-function move_to_trash(req, records){
+async function move_to_trash(req, records){
     var coll = req.app.locals.trashCommentColl
     var formatted = datetime.format('d-m-y H:M:S')
-    for (each_record in records){
-        each_record.deleted = formatted
-        coll.insertOne(each_record)
-    }
+    records.forEach(function(key){
+        console.log("Printing each record", key)
+        key.deleted = formatted
+        coll.insertOne(key, function(err, res){
+            if(err) {
+                console.log("Error from move_to_trash insert", err)
+                throw err
+            }
+            })
+    })
     console.log('All records moved to trash')
 }
 
-function find_all_comments(req, org_name){
+async function find_all_comments(req, org_name){
     var coll = req.app.locals.commentColl
-    var _list = coll.find({"org_name": org_name}, { projection: { _id: 0 } }).toArray(function(err, result){
-        if (err) throw err
-        // console.log(result)
-        // return result
-    })
-    return _list
+    items = await coll.find({"org_name": org_name}, { projection: { _id: 0 } }).toArray()
+    return items
 }
 
-function hard_delete(req, org_name){
+async function hard_delete(req, org_name){
     var coll = req.app.locals.commentColl
-    coll.deleteMany({"org_name": org_name}, function(err, result){
+    await coll.deleteMany({"org_name": org_name}, function(err, result){
         if (err) {
             console.log(err)
             throw err
         }
     })
 }
+// TODO: Add try catch exceptions
 
-app.all('/orgs/:org/comment', function(req, res){
-    console.log("Logging param org:-", req.params.org)
+app.all('/orgs/:org/comment', async function(req, res){
+    var data = []
     if(req.method=='POST'){
-        insert_comment(req, req.params.org, req.body.comment)
+        await insert_comment(req, req.params.org, req.body.comment)
         msg = "The comment has been successfully posted"
     }
     if(req.method=='DELETE'){
-        var records = find_all_comments(req, req.params.org)
-        console.log("Logging records:-", records)
-        move_to_trash(req, records)
-        // hard_delete(req, req.params.org)
+        items = await find_all_comments(req, req.params.org)
+        move_to_trash(req, items)
+        hard_delete(req, req.params.org)
         msg = "The comments have been soft deleted"
     }
-    res.send({"success": true, "error": false, "message": msg})
+    if(req.method=='GET'){
+        items = await find_all_comments(req, req.params.org)
+        data = items
+        msg = "The comments have been fetched."
+    }
+    res.send({"success": true, "error": false, "message": msg, "data": data})
 })
 
 app.get('/', function(req, res){
-    res.send("Hllo world")
+    res.send("Hello world")
     var formatted = datetime.format('d-m-y H:M:S')
     console.log(formatted)
     console.log("Printing request %s", req)
@@ -89,5 +129,5 @@ var server = app.listen(8081, function(){
     var host = server.address().address
     var port = server.address().port
 
-    console.log("Hello world serving at http://%s:%d", host, port)
+    console.log("API serving at http://%s:%d", host, port)
 })
